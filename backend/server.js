390094d8450b570e99ebc4422d66e04eb3a3476e
VigneshBathini <vigneshbathini@gmail.com>
2025-08-08 13:58:51 +0530

    f esbaaaadd

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: ["http://localhost:3001", "https://livemeet-ribm.onrender.com"], // Allow both localhost and network IP
+    origin: "http://localhost:3001"|| "https://livemeet-ribm.onrender.com", // Allow both localhost and network IP
     methods: ["GET", "POST"],
     credentials: true
   }
